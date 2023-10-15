76cfa9cedad6e4a184aa74fca01959eb57ac429b
lenslessai@gmail.com <lenslessai@gmail.com>
2023-10-15 16:44:51 +0200

    add runpod.py

--- downloader.sh
+++ downloader.sh
@@ -3,3 +3,4 @@
 curl -O -L https://raw.githubusercontent.com/lenslessai/start-model-creator-pod/main/pre_start.sh && chmod 777 pre_start.sh
 curl -O -L https://raw.githubusercontent.com/lenslessai/start-model-creator-pod/main/post_start.sh && chmod 777 post_start.sh 
 curl -O -L https://raw.githubusercontent.com/lenslessai/start-model-creator-pod/main/start.sh && chmod 777 start.sh 
+curl -O -L https://raw.githubusercontent.com/lenslessai/start-model-creator-pod/main/runpod.py
