13588276859a5cc9341815eecd82e5d13cf6c090
lenslessai@gmail.com <lenslessai@gmail.com>
2023-10-16 09:36:30 +0200

    fix downloader

--- downloader.sh
+++ downloader.sh
@@ -7,5 +7,6 @@
 #curl -O -L https://raw.githubusercontent.com/lenslessai/start-model-creator-pod/main/post_start.sh && chmod 777 post_start.sh 
 #curl -O -L https://raw.githubusercontent.com/lenslessai/start-model-creator-pod/main/start.sh && chmod 777 start.sh 
 #curl -O -L https://raw.githubusercontent.com/lenslessai/start-model-creator-pod/main/rp_handler.py
-curl -o pre_start.sh -C - -L https://raw.githubusercontent.com/lenslessai/start-model-creator-pod/main/pre_start.sh
-curl -o rp_handler.py -C - -L https://raw.githubusercontent.com/lenslessai/start-model-creator-pod/main/rp_handler.py
+curl https://raw.githubusercontent.com/lenslessai/start-model-creator-pod/main/pre_start.sh > pre_start.sh
+curl https://raw.githubusercontent.com/lenslessai/start-model-creator-pod/main/rp_handler.py > rp_handler.py 
+chmod 777 pre_start.sh
